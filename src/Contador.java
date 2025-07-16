b993da15fb0a8ea070f384550ead2ea2bca0eb8a
faconrado <f.conrado88@hotmail.com>
2025-07-16 18:30:49 -0300

    Add main method to Contador class for parameter input and validation

--- src/Contador.java
+++ src/Contador.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try{
+            contar(parametroUm, parametroDois);  
+            for (parametroUm < parametroDois, parametroUm++){
+
+
+            }
+            
+
+
+        }catch (Exception exception){
+
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
+        }
+        
+    }
+    
 }
