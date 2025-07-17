1b1c52c0835fcfcdf8b9466927d2356fe2f8e042
faconrado <f.conrado88@hotmail.com>
2025-07-16 23:26:00 -0300

    Metodo contar feito e colocado for dentro do metodo.

--- src/Contador.java
+++ src/Contador.java
@@ -10,20 +10,36 @@ public class Contador {
 
         try{
 
-            for (parametroUm = parametroUm; parametroUm < parametroDois; parametroUm++){
+            contar(parametroUm, parametroDois);                
                 
-                System.out.println("Imprimindo o número: " + parametroUm );
-                
-                }
-             
-            
-
-
-        }catch (ParametrosInvalidosException exception){
+        }catch (ParametrosInvalidosException e){
 
             System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
         }
+
+        
         
     }
     
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if (parametroUm >= parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for(int i = 1; i <= contagem; i++){
+            System.out.println("Imprimindo o número: " + (parametroUm + i));
+            
+        }
+    }
+
+    public class ParametrosInvalidosException extends Exception{
+
+    } {
+    
+        
+    }
+
+
 }
